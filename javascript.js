3aec08eb535149160f20dcd7c9efc63ba2f6ecda
louisn973 <louis.n.saunier@gmail.com>
2024-08-22 16:22:18 +0200

    functionn playRound added

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ function getComputerChoice() {
     num = 10*Math.random(3);
     
     if (num<=3) {
-        return "Rock"
+        return "rock"
     }
     else if (num > 3 && num <=6) {
-        return "Paper"
+        return "paper"
     }
     else {
-        return "Scissors"
+        return "scissors"
     }   
 }
 
@@ -16,8 +16,50 @@ function getHumanChoice() {
     choice = prompt("To play the game write the word between : Rock, Paper, Scissors")
     choice = choice.toLowerCase()
     return choice;
-    //chek if the word is correct
-    //if correct return the word as value messages
 }
 
-console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice == computerChoice) {
+        console.log("No one win")
+    }
+    else if ( humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win paper beats rock!");
+        humanScore +=1;
+    }
+    else if ( humanChoice == "paper" && computerChoice =="scissors" ) {
+        console.log("You loose scissors beats paper!");
+        computerScore +=1;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win scissors beats paper !");
+        humanScore+=1;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You loose rock beats scisors!");
+        computerScore +=1;
+    } else if ( humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win rock beats scissors!");
+        humanScore+=1;
+    } else {
+        console.log("You loose paper beats rock!");
+        computerScore+=1;
+    }
+
+}
+
+function playGame() {
+    humanScore = 0;
+    computerScore =0;
+
+
+}
+
+
+
+
+
+
+
+
+
